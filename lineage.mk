24fd80c253eea833d8aa79ad335a41714b278d69
Manish4586 <manish.n.manish45@gmail.com>
2018-05-16 14:06:09 +0530

    First commit

---
+++ lineage.mk
@@ -0,0 +1,17 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := Iris X8
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Lava/IrisX8/device_IrisX8.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := IRISX8
+PRODUCT_NAME := cm_IrisX8
+PRODUCT_BRAND := Lava
+PRODUCT_MODEL := Iris X8
+PRODUCT_MANUFACTURER := Lava
