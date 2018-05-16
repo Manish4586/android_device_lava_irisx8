3199425b42637888737eb0bde2ede5ca07f1f3d3
Manish4586 <Manish.n.manish45@gmail.com>
2018-05-16 19:57:28 +0530

    Fix derp

--- lineage.mk
+++ lineage.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/Lava/IrisX8/device_IrisX8.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := IRISX8
-PRODUCT_NAME := cm_IrisX8
+PRODUCT_NAME := lineage_IrisX8
 PRODUCT_BRAND := Lava
 PRODUCT_MODEL := Iris X8
 PRODUCT_MANUFACTURER := Lava
